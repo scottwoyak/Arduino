b90299003fe050d260f494200cfc352d0df9d446
Scott Woyak <scott.woyak@gmail.com>
2021-08-30 09:17:24 -0400

    added dew point feed

--- libraries/Woyak/SolarTHPWeatherStation.h
+++ libraries/Woyak/SolarTHPWeatherStation.h
@@ -13,6 +13,7 @@
 #include <RunningAverager.h>
 #include <Adafruit_SleepyDog.h>
 #include <Adafruit_LPS35HW.h>
+#include <I2C.h>
 
 // use the Adafruit C1500 WiFi board (via Feather M0 WiFi)
 #define USE_WINC1500
@@ -28,12 +29,6 @@ NTPClient clock(ntpUDP, timeZoneCorrection);
 // ids defined in WiFiSettings.h
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
-// feed timers
-const int WEATHER_INTERVAL = 5;
-const int BATTERY_INTERVAL = 10;
-FeedTimer weatherTimer(&clock, WEATHER_INTERVAL * 60, false);
-FeedTimer batteryTimer(&clock, BATTERY_INTERVAL * 60, false);
-
 // temperature/humidity sensor
 Adafruit_SHT31 sht30;
 
@@ -46,6 +41,7 @@ AdafruitIO_Feed* avgTemperatureFeed;
 AdafruitIO_Feed* enclosureTemperatureFeed;
 AdafruitIO_Feed* humidityRelativeFeed;
 AdafruitIO_Feed* humidityAbsoluteFeed;
+AdafruitIO_Feed* dewPointFeed;
 AdafruitIO_Feed* pressureFeed;
 AdafruitIO_Feed* batteryVoltsFeed;
 AdafruitIO_Feed* batteryChargingVoltsFeed;
@@ -58,28 +54,65 @@ Logger Error(
    new FeedLogHandler(logFeed)
 );
 
+// feed intervals in mins
+const int WEATHER_INTERVAL = 10;
+const int BATTERY_INTERVAL = 15;
+
 AccumulatingAverager temperature(-10, 110);
 RunningAverager avgTemperature(24 * 60 / WEATHER_INTERVAL); // 24 hrs of samples
 AccumulatingAverager enclosureTemperature(-10, 200);
 AccumulatingAverager humidity(0, 100);
 AccumulatingAverager pressure(27, 31);
-AccumulatingAverager batteryVolts(2, 5);
-AccumulatingAverager chargingVolts(2, 5);
+AccumulatingAverager batteryVolts(1, 5);
+AccumulatingAverager chargingVolts(1, 5);
+
+// dewPoint function NOAA
+// reference (1) : http://wahiduddin.net/calc/density_algorithms.htm
+// reference (2) : http://www.colorado.edu/geography/weather_station/Geog_site/about.htm
+//
+double dewPoint(double celsius, double humidity)
+{
+   // (1) Saturation Vapor Pressure = ESGG(T)
+   double RATIO = 373.15 / (273.15 + celsius);
+   double RHS = -7.90298 * (RATIO - 1);
+   RHS += 5.02808 * log10(RATIO);
+   RHS += -1.3816e-7 * (pow(10, (11.344 * (1 - 1 / RATIO))) - 1);
+   RHS += 8.1328e-3 * (pow(10, (-3.49149 * (RATIO - 1))) - 1);
+   RHS += log10(1013.246);
+
+   // factor -3 is to adjust units - Vapor Pressure SVP * humidity
+   double VP = pow(10, RHS - 3) * humidity;
+
+   // (2) DEWPOINT = F(Vapor Pressure)
+   double T = log(VP / 0.61078); // temp var
+   return (241.88 * T) / (17.558 - T);
+}
 
 class SolarTHPWeatherStation {
 private:
    String _name;
    uint8_t _batteryVoltsPin;
    uint8_t _batteryChargingVoltsPin;
+   bool _pressureSensorExists = false;
 
-private:
+   // feed timers
+   FeedTimer weatherTimer;
+   FeedTimer batteryTimer;
+
    const char* dup(String str) {
       return strdup((this->_name + str).c_str());
    }
 
 public:
-   SolarTHPWeatherStation(String name, uint8_t batteryVoltsPin, uint8_t batteryChargingVoltsPin) {
-
+   SolarTHPWeatherStation(
+      String name,
+      uint8_t batteryVoltsPin,
+      uint8_t batteryChargingVoltsPin,
+      uint8_t weatherIntervalMins = WEATHER_INTERVAL
+   ) :
+      weatherTimer(&clock, weatherIntervalMins * 60),
+      batteryTimer(&clock, BATTERY_INTERVAL * 60)
+   {
       this->_name = name;
       this->_batteryVoltsPin = batteryVoltsPin;
       this->_batteryChargingVoltsPin = batteryChargingVoltsPin;
@@ -90,6 +123,7 @@ public:
       enclosureTemperatureFeed = io.feed(this->dup(".enclosure-temperature"));
       humidityRelativeFeed = io.feed(this->dup(".humidity-relative"));
       humidityAbsoluteFeed = io.feed(this->dup(".humidity-absolute"));
+      dewPointFeed = io.feed(this->dup(".dew-point"));
       pressureFeed = io.feed(this->dup(".pressure"));
       batteryVoltsFeed = io.feed(this->dup(".battery-volts"));
       batteryChargingVoltsFeed = io.feed(this->dup(".battery-charging-volts"));
@@ -123,21 +157,31 @@ public:
       Serial.println(io.statusText());
       logFeed->save("Starting " + this->_name + " Weather Sketch");
 
+      Serial.print("sht30.begin()");
       if (sht30.begin(0x44) == false) {
          Error.println("SHT30 (Temperature/Humidity) sensor initialization failed");
       }
-      sht30.heater(false);
+      Serial.println(" - ok");
 
-      if (!lps33hw.begin_I2C()) {
-         Error.println("LPS33HW (Pressure) sensor initialization failed");
+      if (I2C::exists(LPS35HW_I2CADDR_DEFAULT)) {
+         this->_pressureSensorExists = true;
+         Serial.print("lps33hw.begin()");
+         if (!lps33hw.begin_I2C()) {
+            Error.println("LPS33HW (Pressure) sensor initialization failed");
+         }
+         Serial.println(" - ok");
       }
 
+      Serial.print("clock.begin()");
       clock.begin();
       clock.setUpdateInterval(24 * 60 * 60 * 1000);
       clock.update();
+      Serial.println(" - ok");
 
+      Serial.print("timer.begin()");
       weatherTimer.begin();
       batteryTimer.begin();
+      Serial.println(" - ok");
 
       pinMode(this->_batteryVoltsPin, INPUT);
       pinMode(this->_batteryChargingVoltsPin, INPUT);
@@ -165,11 +209,15 @@ public:
       temperature.set(Util::C2F(sht30.readTemperature()));
       humidity.set(sht30.readHumidity());
 
-      lps33hw.takeMeasurement();
-      pressure.set(100 * lps33hw.readPressure() / 3386.39);
-      enclosureTemperature.set(Util::C2F(lps33hw.readTemperature()));
+      if (this->_pressureSensorExists) {
+         Serial.print("Reading Pressure");
+         lps33hw.takeMeasurement();
+         pressure.set(100 * lps33hw.readPressure() / 3386.39);
+         enclosureTemperature.set(Util::C2F(lps33hw.readTemperature()));
+         Serial.println(" - ok");
+      }
 
-      if (weatherTimer.ready()) {
+      if (this->weatherTimer.ready()) {
 
          float tempF = temperature.get();
          float tempC = Util::F2C(tempF);
@@ -177,16 +225,32 @@ public:
          float h = (humidityRelative / 1000) * 3386.39;
          float x = pow(2.718281828, (17.67 * tempC) / (tempC + 243.5));
          float humidityAbsolute = (6.112 * x * h) / (273.15 + tempC);
+         float dp = Util::C2F(dewPoint(tempC, humidityRelative));
+
+         //
+         // Dew Point
+         //
+         // Below 55°F       Air Feels Dry
+         //       55 ~60°F   Air Feels Comfortable
+         //       60 ~64°F   Air Fairly Humid
+         //       65 ~69°F   Humid
+         //       70 ~75°F   Very Humid
+         // Above 75°F       Oppressive
+         //
 
          avgTemperature.set(tempF);
 
          // weather feeds
          temperatureFeed->save(temperature.get());
          avgTemperatureFeed->save(avgTemperature.get());
-         enclosureTemperatureFeed->save(enclosureTemperature.get());
          humidityRelativeFeed->save(humidityRelative);
          humidityAbsoluteFeed->save(humidityAbsolute);
-         pressureFeed->save(pressure.get());
+         dewPointFeed->save(dp);
+
+         if (this->_pressureSensorExists) {
+            pressureFeed->save(pressure.get());
+            enclosureTemperatureFeed->save(enclosureTemperature.get());
+         }
 
          Serial.print("Temp: ");
          Serial.print(tempF);
@@ -208,12 +272,16 @@ public:
          Serial.print("g/m3");
          Serial.println();
 
-         Serial.print("Pressure: ");
-         Serial.print(pressure.get());
-         Serial.print("in");
-         Serial.println();
+         Serial.print("Dew Point: ");
+         Serial.print(dp);
+         Serial.println("F");
 
-
+         if (this->_pressureSensorExists) {
+            Serial.print("Pressure: ");
+            Serial.print(pressure.get());
+            Serial.print("in");
+            Serial.println();
+         }
 
          temperature.reset();
          enclosureTemperature.reset();
@@ -221,7 +289,7 @@ public:
          pressure.reset();
       }
 
-      if (batteryTimer.ready()) {
+      if (this->batteryTimer.ready()) {
 
          // battery stats
          batteryVoltsFeed->save(batteryVolts.get());
@@ -229,6 +297,19 @@ public:
 
          float percent = Util::voltsToPercent(batteryVolts.get());
          batteryPercentFeed->save(percent);
+
+         Serial.print("Charging Volts: ");
+         Serial.print(chargingVolts.get());
+         Serial.println();
+
+         Serial.print("Battery Volts: ");
+         Serial.print(batteryVolts.get());
+         Serial.println();
+
+         Serial.print("Charge Percent: ");
+         Serial.print(percent);
+         Serial.print("%");
+         Serial.println();
 
          chargingVolts.reset();
          batteryVolts.reset();
