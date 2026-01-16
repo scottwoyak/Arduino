a7fa1f16d00913d60f57b772a465f339a7a814c1
Scott Woyak <scott.woyak@gmail.com>
2026-01-16 16:03:42 -0500

    demo of the SH1107 feather wing display

---
+++ Display_SH1107/Display_SH1107.ino
@@ -0,0 +1,38 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+
+Adafruit_SH1107 display(64, 128, &Wire);
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+   pinMode(A0, INPUT_PULLDOWN);
+
+   display.begin(0x3C, true); // Address 0x3C default
+   display.setTextColor(SH110X_WHITE);
+   display.setRotation(1);
+   display.clearDisplay();
+   display.display();
+}
+
+long counter = 0;
+long lastMicros = micros();
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+   long newMicros = micros();
+   double fps = 1000000.0 / (newMicros - lastMicros);
+
+   display.clearDisplay();
+   display.setTextSize(2);
+   display.setCursor(0, 0);
+   display.print(counter++);
+   display.setTextSize(1);
+   display.setCursor(0, display.height() - 8);
+   display.print(fps);
+   display.print(" fps");
+   display.display();
+
+   lastMicros = newMicros;
+}
