37a7849adbafb81708b0c3e96f531c46b792770d
Scott Woyak <scott.woyak@gmail.com>
2026-01-16 17:29:42 -0500

    start of an influx temperature monitor

---
+++ Influx_Temp_Monitor/Influx_Temp_Monitor/Influx_Temp_Monitor.ino
@@ -0,0 +1,40 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+#include "TempSensor.h"
+
+Adafruit_SH1107 display(64, 128, &Wire);
+ITempSensor* sensor;
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+   Wire.begin();
+
+   // start serial port
+   Serial.begin(115200);
+
+   // wait a few seconds for the serial monitor to open
+   while (millis() < 2000 && !Serial)
+   {
+   };
+   delay(500);
+
+   display.begin(0x3C, true); // Address 0x3C default
+   display.setTextColor(SH110X_WHITE);
+   display.setRotation(1);
+   display.clearDisplay();
+   display.display();
+
+   sensor = TempSensorFactory::create();
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+   display.clearDisplay();
+   display.setTextSize(2);
+   display.setCursor(0, 0);
+   display.print("Scott");
+   display.print(sensor->readTemperature());
+   display.display();
+}
