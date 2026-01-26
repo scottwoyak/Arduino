3d58963c0ddea2ad4802bf3c8c166955e1c0c8de
Scott Woyak <scott.woyak@gmail.com>
2026-01-26 11:51:13 -0500

    fixes

--- ButtonDisplay/ButtonDisplay.ino
+++ ButtonDisplay/ButtonDisplay.ino
@@ -1,48 +1,37 @@
 
-#if defined(ADAFRUIT_FEATHER_M0)
-
-#include "Feather_M0_OLED.h"
-const Color TextColor = Color::WHITE;
-const Color ValueColor = Color::WHITE;
-const Color InfoColor = Color::WHITE;
-Feather_M0_OLED feather;
-
-#elif defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S3_TFT)
-
-#include "Feather_ESP32_S3.h"
-const Color TextColor = Color::WHITE;
-const Color ValueColor = Color::YELLOW;
-const Color InfoColor = Color::GRAY;
-Feather_ESP32_S3 feather;
-
-#endif
-
+#include "Feather.h"
 #include "Stopwatch.h"
-#include "RunningAverager.h"
+
+Feather feather;
+
+Stopwatch sw(false);
+uint16_t lastCount = 0;
+
+Button button(6);
+//Button& button = feather.buttonA;
 
 void setup()
 {
    Serial.begin(115200);
    feather.begin();
+   button.begin();
 }
-
-Stopwatch sw(false);
-uint16_t lastCount = 0;
-Button& button = feather.buttonA;
 
 void loop()
 {
-   if ((button.wasPressed() && sw.isRunning() == false) || button.isCurrentlyPressed())
+   if ((button.wasPressed() && sw.isRunning() == false) || button.isPressed())
    {
       sw.reset();
       sw.start();
    }
 
-   feather.display.setTextSize((uint8_t)TextSize::MEDIUM);
+   feather.setTextSize(TextSize::MEDIUM);
    feather.setCursor(0, 0);
 
+   feather.print("Pin: ");
+   feather.println(button.getPin(), 2, ValueColor);
    feather.print(" Is: ");
-   feather.println(button.isCurrentlyPressed() ? "True" : "False", 5, ValueColor);
+   feather.println(button.isPressed() ? "True" : "False", 5, ValueColor);
    feather.print("Was: ");
    feather.println(button.wasPressed() ? "True" : "False", 5, ValueColor);
    feather.print("  #: ");
