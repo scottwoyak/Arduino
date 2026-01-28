036946aa5ba71c228eb773f798f12e2ced14046f
Scott Woyak <scott.woyak@gmail.com>
2026-01-28 08:29:18 -0500

    added some more helpers

--- libraries/Woyak/Feather_ESP32_S3.h
+++ libraries/Woyak/Feather_ESP32_S3.h
@@ -7,10 +7,38 @@
 #include <string>
 #include <Preferences.h>
 
+//
+// Annoyingling, Adafruit has a lot of nifty properties in AdafruitGFX that you can set, but not 
+// get. This class is just a wrapper that exposes them
+//
+class SW_ST7789 : public Adafruit_ST7789
+{
+public:
+   SW_ST7789(int8_t cs, int8_t dc, int8_t mosi, int8_t sclk, int8_t rst = -1) : Adafruit_ST7789(cs, dc, mosi, sclk, rst)
+   {
+   }
+
+   SW_ST7789(int8_t cs, int8_t dc, int8_t rst) : Adafruit_ST7789(cs, dc, rst)
+   {
+   }
+
+   uint8_t charW()
+   {
+      // 6 is the baseline character width that is scaled for larger text
+      return 6 * textsize_x;
+   }
+
+   uint8_t charH()
+   {
+      // 8 is the baseline character width that is scaled for larger text
+      return 8 * textsize_y;
+   }
+};
+
 class Feather_ESP32_S3 : public ArduinoWithDisplay
 {
 public:
-   Adafruit_ST7789 display;
+   SW_ST7789 display;
    Button buttonA;
    Preferences preferences;
 
@@ -43,17 +71,47 @@ public:
    {
       digitalWrite(TFT_BACKLITE, HIGH);
    }
+
    void displayOff()
    {
       digitalWrite(TFT_BACKLITE, LOW);
    }
+
    bool isDisplayOn()
    {
       return digitalRead(TFT_BACKLITE) == HIGH;
    }
 
+   void displayLevel(float level)
+   {
+      uint8_t pwm = constrain(level * 255, 0, 255);
+      analogWrite(TFT_BACKLITE, pwm);
+   }
+
    void displayDisplay()
    {
       // nothing to do here. The TFT isn't double buffered
+   }
+
+   uint8_t charW()
+   {
+      return display.charW();
+   }
+
+   uint8_t charH()
+   {
+      return display.charH();
+   }
+
+   void printR(const char* str, Color textColor = Color::WHITE, Color backgroundColor = Color::BLACK)
+   {
+      setCursorX(-strlen(str) * charW());
+      print(str, textColor, backgroundColor);
+   }
+
+   void printlnR(const char* str, Color textColor = Color::WHITE, Color backgroundColor = Color::BLACK)
+   {
+      setCursorX(-strlen(str) * charW());
+      println(str, textColor, backgroundColor);
    }
 };
