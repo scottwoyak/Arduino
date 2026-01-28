f8520af154f33a32bedeca2dd332eed8c0b692b5
Scott Woyak <scott.woyak@gmail.com>
2026-01-28 15:55:43 -0500

    included <limits> for boards that don't automatically do it

--- libraries/Woyak/RotaryEncoder.h
+++ libraries/Woyak/RotaryEncoder.h
@@ -2,6 +2,7 @@
 
 #include "Arduino.h"
 #include "Button.h"
+#include <limits>
 
 //-------------------------------------------------------------------------------------------------
 class RotaryEncoder
