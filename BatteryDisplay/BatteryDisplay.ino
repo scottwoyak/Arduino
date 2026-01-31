f60b4efea11e57d6f5dc4e5c4f0a301e92e359db
Scott Woyak <scott.woyak@gmail.com>
2026-01-31 13:16:07 -0500

    improved visuals

--- BatteryDisplay/BatteryDisplay.ino
+++ BatteryDisplay/BatteryDisplay.ino
@@ -8,7 +8,7 @@ Adafruit_MAX17048 battery;
 
 Format batteryVoltsFormat("#.#v");
 Format percentFormat("###%");
-Format chargeRateFormat("####%/hr");
+Format chargeRateFormat("+###%/hr");
 
 void setup()
 {
@@ -26,20 +26,30 @@ void loop()
    feather.setCursor(0, 0);
    feather.setTextSize(TextSize::MEDIUM);
    feather.println("Battery Info", Color::HEADING);
-   feather.moveCursorY(feather.charH() / 2);
+   feather.moveCursorY(feather.charH() / 4);
 
-   feather.print("Volts: ", Color::WHITE);
+   feather.print("Volts: ", Color::LABEL);
    feather.println(battery.cellVoltage(), batteryVoltsFormat, Color::VALUE);
    feather.moveCursorY(feather.charH() / 4);
 
-   feather.print("State: ", Color::WHITE);
+   feather.print("State: ", Color::LABEL);
    uint16_t x = feather.display.getCursorX();
    feather.println(battery.cellPercent(), percentFormat, Color::VALUE);
    feather.moveCursorY(feather.charH() / 4);
 
-   feather.print(" Rate: ", Color::WHITE);
-   feather.setCursorX(x);
-   feather.moveCursorY((CharSize::MEDIUM_H - CharSize::SMALL_H) / 2);
+   feather.print(" Rate: ", Color::LABEL);
    feather.setTextSize(TextSize::SMALL);
-   feather.print(battery.chargeRate(), chargeRateFormat, Color::VALUE);
+   feather.setCursorX(x);
+   float chargeRate = battery.chargeRate();
+   feather.println(chargeRate, chargeRateFormat, Color::VALUE);
+   feather.setCursorX(x);
+
+   if (fabs(chargeRate) >= 1)
+   {
+      feather.println(chargeRate > 0 ? "Charging" : "Draining", Color::VALUE2);
+   }
+   else
+   {
+      feather.println("        ");
+   }
 }
