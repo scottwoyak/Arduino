fa9fcd25c01434f2b07af913cdb5832dfbe7213d
Scott Woyak <scott.woyak@gmail.com>
2026-03-08 17:33:38 -0400

    Initial tool for creating TFT_eSPI fonts

---
+++ SmoothFontCreator/MainForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace SmoothFontCreator;
+
+partial class MainForm
+{
+   /// <summary>
+   ///  Required designer variable.
+   /// </summary>
+   private System.ComponentModel.IContainer components = null;
+
+   /// <summary>
+   ///  Clean up any resources being used.
+   /// </summary>
+   /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+   protected override void Dispose(bool disposing)
+   {
+      if (disposing && (components != null))
+      {
+         components.Dispose();
+      }
+      base.Dispose(disposing);
+   }
+
+   #region Windows Form Designer generated code
+
+   /// <summary>
+   ///  Required method for Designer support - do not modify
+   ///  the contents of this method with the code editor.
+   /// </summary>
+   private void InitializeComponent()
+   {
+      tabControl1 = new TabControl();
+      tabPage1 = new TabPage();
+      TestCharTextBox = new TextBox();
+      TestCharsTextBox = new TextBox();
+      FontComboBox = new ComboBox();
+      MetricsPanel = new Panel();
+      CharPanel = new Panel();
+      AllCharsPanel = new Panel();
+      tabPage2 = new TabPage();
+      ASCIILabel = new Label();
+      HexLabel = new Label();
+      tabControl1.SuspendLayout();
+      tabPage1.SuspendLayout();
+      SuspendLayout();
+      // 
+      // tabControl1
+      // 
+      tabControl1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+      tabControl1.Controls.Add(tabPage1);
+      tabControl1.Controls.Add(tabPage2);
+      tabControl1.Location = new Point(12, 12);
+      tabControl1.Name = "tabControl1";
+      tabControl1.SelectedIndex = 0;
+      tabControl1.Size = new Size(1514, 981);
+      tabControl1.TabIndex = 0;
+      // 
+      // tabPage1
+      // 
+      tabPage1.Controls.Add(HexLabel);
+      tabPage1.Controls.Add(ASCIILabel);
+      tabPage1.Controls.Add(TestCharTextBox);
+      tabPage1.Controls.Add(TestCharsTextBox);
+      tabPage1.Controls.Add(FontComboBox);
+      tabPage1.Controls.Add(MetricsPanel);
+      tabPage1.Controls.Add(CharPanel);
+      tabPage1.Controls.Add(AllCharsPanel);
+      tabPage1.Location = new Point(4, 34);
+      tabPage1.Name = "tabPage1";
+      tabPage1.Padding = new Padding(3);
+      tabPage1.Size = new Size(1506, 943);
+      tabPage1.TabIndex = 0;
+      tabPage1.Text = "tabPage1";
+      tabPage1.UseVisualStyleBackColor = true;
+      // 
+      // TestCharTextBox
+      // 
+      TestCharTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+      TestCharTextBox.Location = new Point(1350, 360);
+      TestCharTextBox.Name = "TestCharTextBox";
+      TestCharTextBox.Size = new Size(150, 31);
+      TestCharTextBox.TabIndex = 5;
+      // 
+      // TestCharsTextBox
+      // 
+      TestCharsTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+      TestCharsTextBox.Location = new Point(1225, 45);
+      TestCharsTextBox.Multiline = true;
+      TestCharsTextBox.Name = "TestCharsTextBox";
+      TestCharsTextBox.Size = new Size(275, 309);
+      TestCharsTextBox.TabIndex = 4;
+      TestCharsTextBox.Text = "0@123.xyz\r\n0@123.xyz\r\n0@123.xyz\r\n0@123.xyz\r\n0@123.xyz\r\n0@123.xyz\r\n0@123.xyz";
+      // 
+      // FontComboBox
+      // 
+      FontComboBox.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+      FontComboBox.FormattingEnabled = true;
+      FontComboBox.Location = new Point(1225, 6);
+      FontComboBox.Name = "FontComboBox";
+      FontComboBox.Size = new Size(275, 33);
+      FontComboBox.TabIndex = 3;
+      // 
+      // MetricsPanel
+      // 
+      MetricsPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+      MetricsPanel.BackColor = Color.Black;
+      MetricsPanel.Location = new Point(616, 223);
+      MetricsPanel.Name = "MetricsPanel";
+      MetricsPanel.Size = new Size(603, 714);
+      MetricsPanel.TabIndex = 2;
+      // 
+      // CharPanel
+      // 
+      CharPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+      CharPanel.BackColor = Color.DimGray;
+      CharPanel.Location = new Point(6, 223);
+      CharPanel.Name = "CharPanel";
+      CharPanel.Size = new Size(604, 714);
+      CharPanel.TabIndex = 1;
+      // 
+      // AllCharsPanel
+      // 
+      AllCharsPanel.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+      AllCharsPanel.BackColor = Color.Black;
+      AllCharsPanel.Location = new Point(6, 6);
+      AllCharsPanel.Name = "AllCharsPanel";
+      AllCharsPanel.Size = new Size(1213, 211);
+      AllCharsPanel.TabIndex = 0;
+      // 
+      // tabPage2
+      // 
+      tabPage2.Location = new Point(4, 34);
+      tabPage2.Name = "tabPage2";
+      tabPage2.Padding = new Padding(3);
+      tabPage2.Size = new Size(1506, 943);
+      tabPage2.TabIndex = 1;
+      tabPage2.Text = "tabPage2";
+      tabPage2.UseVisualStyleBackColor = true;
+      // 
+      // ASCIILabel
+      // 
+      ASCIILabel.AutoSize = true;
+      ASCIILabel.Location = new Point(1350, 408);
+      ASCIILabel.Name = "ASCIILabel";
+      ASCIILabel.Size = new Size(59, 25);
+      ASCIILabel.TabIndex = 6;
+      ASCIILabel.Text = "label1";
+      // 
+      // HexLabel
+      // 
+      HexLabel.AutoSize = true;
+      HexLabel.Location = new Point(1350, 457);
+      HexLabel.Name = "HexLabel";
+      HexLabel.Size = new Size(59, 25);
+      HexLabel.TabIndex = 7;
+      HexLabel.Text = "label1";
+      // 
+      // MainForm
+      // 
+      AutoScaleDimensions = new SizeF(10F, 25F);
+      AutoScaleMode = AutoScaleMode.Font;
+      ClientSize = new Size(1538, 1005);
+      Controls.Add(tabControl1);
+      Name = "MainForm";
+      Text = "Smooth Font Creator for TFT_eSPI";
+      tabControl1.ResumeLayout(false);
+      tabPage1.ResumeLayout(false);
+      tabPage1.PerformLayout();
+      ResumeLayout(false);
+   }
+
+   #endregion
+
+   private TabControl tabControl1;
+   private TabPage tabPage1;
+   private TabPage tabPage2;
+   private Panel AllCharsPanel;
+   private Panel CharPanel;
+   private Panel MetricsPanel;
+   private ComboBox FontComboBox;
+   private TextBox TestCharsTextBox;
+   private TextBox TestCharTextBox;
+   private Label HexLabel;
+   private Label ASCIILabel;
+}
---
+++ SmoothFontCreator/MainForm.cs
@@ -0,0 +1,305 @@
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+
+namespace SmoothFontCreator;
+
+class Metric
+{
+   public uint xMin = uint.MaxValue;
+   public uint xMax = uint.MinValue;
+   public uint yMin = uint.MaxValue;
+   public uint yMax = uint.MinValue;
+   public uint Width => xMax - xMin;
+   public uint Height => yMax - yMin;
+
+   public void update(uint x, uint y)
+   {
+      xMin = uint.Min(xMin, x);
+      xMax = uint.Max(xMax, x);
+      yMin = uint.Min(yMin, y);
+      yMax = uint.Max(yMax, y);
+   }
+}
+
+class ActualMetrics
+{
+   public Metric CellMetric = new();
+   public Metric CharMetric = new();
+
+   public float Height => CharMetric.Height;
+   public float Width => CharMetric.Width;
+   public float LineSpacing => CellMetric.Height;
+   public float Top => CharMetric.yMin - CellMetric.yMin;
+   public float Bottom => Top + Height;
+
+   public ActualMetrics(Bitmap b)
+   {
+      for (uint x = 0; x < b.Width; x++)
+      {
+         for (uint y = 0; y < b.Height; y++)
+         {
+            Color pixelColor = b.GetPixel((int)x, (int)y);
+            if (pixelColor.A != 0)
+            {
+               CellMetric.update(x, y);
+               if (pixelColor.ToArgb() != Color.Black.ToArgb())
+               {
+                  CharMetric.update(x, y);
+               }
+            }
+         }
+      }
+
+      Debug.WriteLine($"Cell Size: {CellMetric.Width}px X {CellMetric.Height}px");
+      Debug.WriteLine($"Char Size: {CharMetric.Width}px X {CharMetric.Height}px");
+   }
+}
+
+class GdiMetrics
+{
+   public float Height;
+   public float Width;
+   public float Ascent;
+   public float Descent;
+   public float LineSpacing;
+   public float Top;
+   public float Baseline;
+   public float Bottom;
+
+   public GdiMetrics(Font font, Graphics g)
+   {
+      float heightEm = font.FontFamily.GetEmHeight(FontStyle.Regular);
+      float ascentEm = font.FontFamily.GetCellAscent(FontStyle.Regular);
+      float descentEm = font.FontFamily.GetCellDescent(FontStyle.Regular);
+      float lineSpacingEm = font.FontFamily.GetLineSpacing(FontStyle.Regular);
+
+      Height = font.GetHeight(g);
+      Width = TextRenderer.MeasureText("0", font, new Size(1000, 1000), TextFormatFlags.NoPadding).Width;
+      Ascent = (ascentEm / lineSpacingEm) * Height;
+      Descent = (descentEm / lineSpacingEm) * Height;
+      LineSpacing = (Height / heightEm) * lineSpacingEm;
+
+      Top = Height - font.Size;
+      Baseline = Ascent;
+      Bottom = Ascent + Descent;
+   }
+}
+
+public partial class MainForm : Form
+{
+
+   public MainForm()
+   {
+      InitializeComponent();
+
+      FontComboBox.Items.Add("Arial");
+      FontComboBox.Items.Add("Times New Roman");
+      FontComboBox.Items.Add("Roboto Mono");
+      FontComboBox.Items.Add("Roboto");
+      FontComboBox.SelectedIndex = 0;
+      FontComboBox.SelectedValueChanged += FontComboBox_SelectedValueChanged;
+
+
+      CharPanel.Paint += CharPanel_Paint;
+      AllCharsPanel.Paint += AllCharsPanel_Paint;
+      MetricsPanel.Paint += MetricsPanel_Paint;
+
+      TestCharsTextBox.TextChanged += (s, e) =>
+      {
+         CharPanel.Invalidate();
+         MetricsPanel.Invalidate();
+      };
+
+      CharPanel.MouseWheel += CharPanel_MouseWheel;
+   }
+
+   private void CharPanel_MouseWheel(object sender, MouseEventArgs e)
+   {
+      char c  = TestCharTextBox.Text.Length > 0 ? TestCharTextBox.Text[0] : 'A';
+      if (e.Delta > 0)
+      {
+         c--;
+      }
+      else if (e.Delta < 0)
+      {
+         c++;
+      }
+      if (c < 0x20)
+      {
+         c = (char) 0x7E;
+      }
+      else if (c > 0x7E)
+      {
+         c = (char) 0x20;
+      }
+
+      TestCharTextBox.Text = c.ToString();
+      ASCIILabel.Text = ((int)c).ToString();
+      HexLabel.Text = $"0x{(int)c:X2}";
+
+      CharPanel.Invalidate();
+   }
+
+   private void FontComboBox_SelectedValueChanged(object sender, EventArgs e)
+   {
+      AllCharsPanel.Invalidate();
+      CharPanel.Invalidate();
+      MetricsPanel.Invalidate();
+   }
+
+   private void PrintFontMetrics(Font font, Graphics g)
+   {
+      float heightEm = font.FontFamily.GetEmHeight(FontStyle.Regular);
+      float ascentEm = font.FontFamily.GetCellAscent(FontStyle.Regular);
+      float descentEm = font.FontFamily.GetCellDescent(FontStyle.Regular);
+      float lineSpacingEm = font.FontFamily.GetLineSpacing(FontStyle.Regular);
+
+      float heightPx = font.GetHeight(g);
+      //float heightPx = font.Height;
+      //float widthPx = TextRenderer.MeasureText("0", font).Width;
+      float widthPx = TextRenderer.MeasureText("0", font, new Size(1000, 1000), TextFormatFlags.NoPadding).Width;
+      float ascentPx = (ascentEm / lineSpacingEm) * heightPx;
+      float descentPx = (descentEm / lineSpacingEm) * heightPx;
+      float lineSpacingPx = (heightPx / heightEm) * lineSpacingEm;
+
+      Debug.WriteLine($"{font.FontFamily.GetName(0)}, {font.SizeInPoints}pt");
+
+      Debug.WriteLine($"font.Size: {font.Size}");
+      Debug.WriteLine($"font.Height: {font.Height}");
+      Debug.WriteLine($"font.GetHeight(Graphics): {font.GetHeight(g)}");
+
+      /*
+      Debug.WriteLine($"fontFamily.GetEmHeight(): {font.FontFamily.GetEmHeight(FontStyle.Regular)}");
+      Debug.WriteLine($"fontFamily.GetCellAscent(): {font.FontFamily.GetCellAscent(FontStyle.Regular)}");
+      Debug.WriteLine($"fontFamily.GetCellDescent(): {font.FontFamily.GetCellDescent(FontStyle.Regular)}");
+      Debug.WriteLine($"fontFamily.GetLineSpacing(): {font.FontFamily.GetLineSpacing(FontStyle.Regular)}");
+      Debug.WriteLine("");
+      */
+
+      Debug.WriteLine($"Height: {heightPx}px");
+      Debug.WriteLine($"Width: {widthPx}px");
+      Debug.WriteLine("");
+      /*
+      Debug.WriteLine($"Height: {heightEm}em");
+      Debug.WriteLine($"Ascent: {ascentEm}em");
+      Debug.WriteLine($"Descent: {descentEm}em");
+      Debug.WriteLine($"LineSpacing: {lineSpacingEm}em");
+      Debug.WriteLine("");
+      */
+      Debug.WriteLine($"Height: {heightPx}px");
+      Debug.WriteLine($"Ascent: {ascentPx}px");
+      Debug.WriteLine($"Descent: {descentPx}px");
+      Debug.WriteLine($"LineSpacing: {lineSpacingPx}px");
+      Debug.WriteLine("");
+      Debug.WriteLine($"Panel Size: {CharPanel.Width}px X {CharPanel.Height}px");
+   }
+
+   private void AllCharsPanel_Paint(object sender, PaintEventArgs e)
+   {
+      FontFamily fontFamily = new FontFamily(FontComboBox.Text);
+
+      Font font = new Font(
+         fontFamily,
+         32,
+         FontStyle.Regular,
+         GraphicsUnit.Pixel);
+
+      string chars = "abcdefghijklmnopqrstuvwxyz\nABCDEFGHIJKLMNOPQRSTUVWXYZ\n0123456789!@#$%^&*()_+-=~`[]{}|;:'\",.<>/?";
+
+      e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+      TextRenderer.DrawText(e.Graphics, chars, font, new Point(0, 0), Color.White, Color.Transparent, TextFormatFlags.NoPadding);
+   }
+
+   private void CharPanel_Paint(object sender, PaintEventArgs e)
+   {
+      if (TestCharTextBox.Text.Length > 0)
+      {
+         using Bitmap bitmap = new(CharPanel.Width, CharPanel.Height);
+         Graphics g = Graphics.FromImage(bitmap);
+         g.Clear(Color.Transparent);
+
+         FontFamily fontFamily = new(FontComboBox.Text);
+         Font font = new Font(
+            fontFamily,
+            512,
+            FontStyle.Regular,
+            GraphicsUnit.Pixel);
+
+         PrintFontMetrics(font, g);
+
+         string testChar = TestCharTextBox.Text.Substring(0, 1);
+         float heightPx = font.GetHeight(e.Graphics);
+         float widthPx = TextRenderer.MeasureText(testChar, font, new Size(1000, 1000), TextFormatFlags.NoPadding).Width;
+
+         g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+         Point pt = new((int) (CharPanel.Width - widthPx)/2, (int) (CharPanel.Height-heightPx)/2);
+         TextRenderer.DrawText(g, testChar, font, pt, Color.White, Color.Black, TextFormatFlags.NoPadding);
+
+         e.Graphics.DrawImage(bitmap, new Point(0,0));
+
+         ActualMetrics computedMetrics = new(bitmap);
+
+         // draw what Windows reports on the left
+         GdiMetrics gdiMetrics = new GdiMetrics(font, g);
+
+         Pen topPen = new(Color.Pink, 1);
+         Pen baselinePen = new(Color.LightCoral, 1);
+         Pen bottomPen = new(Color.Cyan, 1);
+         topPen.DashStyle = DashStyle.Dot;
+         baselinePen.DashStyle = DashStyle.Dot;
+         bottomPen.DashStyle = DashStyle.Dot;
+
+         float x1 = 0;
+         float x2 = CharPanel.Width/2;
+         e.Graphics.DrawLine(topPen, x1, pt.Y + gdiMetrics.Top, x2, pt.Y + gdiMetrics.Top);
+         e.Graphics.DrawLine(baselinePen, x1, pt.Y + gdiMetrics.Baseline, x2, pt.Y + gdiMetrics.Baseline);
+         e.Graphics.DrawLine(bottomPen, x1, pt.Y + gdiMetrics.Bottom, x2, pt.Y + gdiMetrics.Bottom);
+
+         float top = pt.Y + computedMetrics.Top;
+         float bottom = pt.Y + computedMetrics.Bottom;
+         x1 = CharPanel.Width / 2;
+         x2 = CharPanel.Width;
+
+         // draw what we calculated on the right
+         e.Graphics.DrawLine(topPen, x1, top, x2, top);
+         e.Graphics.DrawLine(bottomPen, x1, bottom, x2, bottom);
+      }
+   }
+
+   private void MetricsPanel_Paint(object sender, PaintEventArgs e)
+   {
+      FontFamily fontFamily = new FontFamily(FontComboBox.Text);
+      Font font = new Font(
+         fontFamily,
+         128,
+         FontStyle.Regular,
+         GraphicsUnit.Pixel);
+
+
+
+      e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+      TextRenderer.DrawText(e.Graphics, TestCharsTextBox.Text, font, new Point(0, 0), Color.White, Color.Black, TextFormatFlags.NoPadding);
+
+
+      //float heightEm = font.FontFamily.GetEmHeight(FontStyle.Regular);
+      float ascentEm = font.FontFamily.GetCellAscent(FontStyle.Regular);
+      float descentEm = font.FontFamily.GetCellDescent(FontStyle.Regular);
+      float lineSpacingEm = font.FontFamily.GetLineSpacing(FontStyle.Regular);
+
+      float heightPx = font.GetHeight(e.Graphics);
+      float ascentPx = (ascentEm / lineSpacingEm) * heightPx;
+      float descentPx = (descentEm / lineSpacingEm) * heightPx;
+
+      float top = heightPx - font.Size;
+      float baseline = ascentPx;
+      float bottom = ascentPx + descentPx;
+
+      for (int i = 0; i < 8; i++)
+      {
+         float start = i * (bottom);
+         e.Graphics.DrawLine(new Pen(Color.Purple, 1), 0, start + top, MetricsPanel.Width, start + top);
+         e.Graphics.DrawLine(new Pen(Color.LightCoral, 1), 0, start + baseline, MetricsPanel.Width, start + baseline);
+         e.Graphics.DrawLine(new Pen(Color.Cyan, 1), 0, start + bottom, MetricsPanel.Width, start + bottom);
+      }
+   }
+}
