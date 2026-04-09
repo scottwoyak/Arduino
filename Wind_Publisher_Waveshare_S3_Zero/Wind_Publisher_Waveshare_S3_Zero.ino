a5492c39336d014281e6800c5d067362417e0117
Scott Woyak <scott.woyak@gmail.com>
2026-04-09 13:32:06 -0400

    added colored LED display to indicate status at startup

--- Wind_Publisher_Waveshare_S3_Zero/Wind_Publisher_Waveshare_S3_Zero.ino
+++ Wind_Publisher_Waveshare_S3_Zero/Wind_Publisher_Waveshare_S3_Zero.ino
@@ -20,16 +20,18 @@ WindMeter wind(WIND_PIN, 0);
 
 void setup()
 {
-   SerialX::begin();
-   wind.begin();
-
    pinMode(LED_BUILTIN, OUTPUT);
+   rgbLedWrite(LED_BUILTIN, 255, 255, 255);
+   delay(1000); // just to show the LED
 
-   // Connect to WiFi
-   wifi.addAP(WIFI_SSID, WIFI_PASSWORD);
-
+   SerialX::begin();
    Serial.println("Wind Publisher");
 
+   wind.begin();
+
+   // Connect to WiFi
+   rgbLedWrite(LED_BUILTIN, 0, 0, 255); // blue while connecting to WiFi
+   wifi.addAP(WIFI_SSID, WIFI_PASSWORD);
    Serial.print("WiFi...");
    while (wifi.run() != WL_CONNECTED)
    {
@@ -37,8 +39,10 @@ void setup()
    }
    Serial.println("OK");
 
+   rgbLedWrite(LED_BUILTIN, 0, 255, 0); // green while connecting to the server
    client.setCallbacks(onConnected, onDisconnected, nullptr, onError, nullptr);
    client.beginSSL(TELEMETRY_HOST, TELEMETRY_PORT);
+   delay(1000); // just to show the LED
 }
 
 void onConnected()
@@ -48,27 +52,42 @@ void onConnected()
 
 void onDisconnected()
 {
+   rgbLedWrite(LED_BUILTIN, 255, 0, 0); // red on error
    Serial.println("Disconnected");
-   delay(1000); // time for Serial to print
+   delay(1000); // time for Serial to print and LED to show
    Util::reset();
 }
 
 void onError(std::string msg)
 {
+   rgbLedWrite(LED_BUILTIN, 255, 0, 0); // red on error
    Serial.print("Error: ");
    Serial.println(msg.c_str());
+   delay(1000); // time for Serial to print and LED to show
    Util::reset();
 }
 
 void loop()
 {
-   // Waveshare crashes if we set the LED value in the interrupt, so we manually do it
-   rgbLedWrite(LED_BUILTIN, wind.ledState() ? 255 : 0, 0, 0);
+   if (client.isStarted())
+   {
+      // Waveshare crashes if we set the LED value in the interrupt, so we manually do it
+      if (wind.ledState())
+      {
+         // orange
+         rgbLedWrite(LED_BUILTIN, 255, 165, 0);
+      }
+      else
+      {
+         digitalWrite(LED_BUILTIN, LOW);
+      }
 
-   // without a delay, the waveshare crashes
-   delay(1);
+      // without a delay, the waveshare crashes
+      delay(1);
 
-   float speed = wind.getSpeed();
-   client.setValue(speed);
+      float speed = wind.getSpeed();
+      client.setValue(speed);
+   }
+
    client.loop(); // Continuously poll for events and maintain connection
 }
