9eea0b31a48ee3a7f32d4731f9d7199d81d23413
Scott Woyak <scott.woyak@gmail.com>
2026-05-25 09:07:01 -0400

    Capitalization changes for LED

---
+++ LED/LED.ino
@@ -0,0 +1,14 @@
+#include "Led.h"
+
+Led led(BUILTIN_LED);
+
+void setup()
+{
+   led.begin();
+   led.blink(200);
+}
+
+// the loop function runs over and over again until power down or reset
+void loop()
+{
+}
