85c13470111e2ce328f26927c29708a9c2eca11a
Scott Woyak <scott.woyak@gmail.com>
2026-09-08 17:11:00 -0400

    Updated to work with the Waveshare Zero board which doesn't have a built-in led

--- LED/LED.ino
+++ LED/LED.ino
@@ -1,6 +1,16 @@
+// The WaveShare ESP32-S3-Zero has no built-in LED, so this uses the sensor board's
+// general-purpose LED pin (see WaveShare_ESP32_S3_Zero_Sensors::DEFAULT_LED_PIN)
+// instead of BUILTIN_LED.
 #include "LED.h"
+#include "Waveshare_ESP32_S3_Zero.h"
 
-LED led(BUILTIN_LED);
+#ifdef ARDUINO_WAVESHARE_ESP32_S3_ZERO
+constexpr uint8_t LED_PIN = WaveShare_ESP32_S3_Zero_Sensors::DEFAULT_LED_PIN;
+#else
+constexpr uint8_t LED_PIN = BUILTIN_LED;
+#endif
+
+LED led(LED_PIN);
 
 void setup()
 {
